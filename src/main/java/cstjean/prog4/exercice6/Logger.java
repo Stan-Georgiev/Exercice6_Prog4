285b7a64a9463fb85755908d5deec4e2d68a3f29
Tristan086 <6292338@cstjean.qc.ca>
2026-03-30 09:23:54 -0400

    Logger 2

--- src/main/java/cstjean/prog4/exercice6/Logger.java
+++ src/main/java/cstjean/prog4/exercice6/Logger.java
@@ -1,8 +1,27 @@
 package cstjean.prog4.exercice6;
 
-public class Logger {
+import java.util.ArrayList;
+
+public abstract class Logger {
+    private ArrayList<Logger> loggers;
     public static int NONE = 0;
     public static int INFO = 1;
     public static int WARNING = 2;
     public static int ERROR = 4;
+
+    public Logger() {
+        loggers = new ArrayList<Logger>();
+    }
+    public void log(String message, int level) {
+        if (loggers.size() == 0) {
+            System.out.println("Aucuns loggers trouvés");
+            return;
+        }
+        for (int i = 0; i < loggers.size(); i++) {
+            loggers.get(i).log(message, level);
+        }
+    }
+    public void addLogger(Logger logger) {
+        loggers.add(logger);
+    }
 }
